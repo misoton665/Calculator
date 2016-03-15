b0dba1ac320348e74014950cdaa506b9dcf3ed61
misoton665 <misoton998@hotmail.co.jp>
2016-03-15 16:53:22 +0900

    Update CUI

--- main.rb
+++ main.rb
@@ -4,6 +4,7 @@ require './calculator_node_classes'
 Treetop.load 'calculator'
 
 loop {
+  print "$> "
   input = STDIN.gets
   tree = CalculatorParser.new.parse(input)
   puts "=> #{tree.eval.to_s}"
